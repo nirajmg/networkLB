fab1224b7a8ecb7ee339f367042d94a1a8a6c6bd
nirajmg <niraj@engr2-26-142-dhcp.int.colorado.edu>
2022-10-17 17:20:58 -0600

    fix bug k8s client

--- nlb/k8s/pods.go
+++ nlb/k8s/pods.go
@@ -2,18 +2,30 @@ package k8s
 
 import (
 	"context"
+	"flag"
+	"path/filepath"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
 )
 
 var Client *kubernetes.Clientset
 
 func NewClient() error {
-	config, err := rest.InClusterConfig()
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	// use the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
 	if err != nil {
-		return err
+		panic(err.Error())
 	}
 	// creates the clientset
 	Client, err = kubernetes.NewForConfig(config)
