b33ce7acb5b1b9079f2d61a219c7ada8d9acbdb1
hlhanhl <huilin.han@colorado.edu>
2022-11-12 14:28:09 -0700

    ip hash initial

--- nlb/algo/ip_hash.go
+++ nlb/algo/ip_hash.go
@@ -1,21 +1,49 @@
-package main
+package algo
 
 import (
 	//"fmt"
 	"strings"
+	"strconv"
 	"container/list"
 )
 
 type Ip_Hash struct{
-	func hash(ip string, n int)(string,error){
-		//parse string by .
-		//convert string to ints 
-		//add ints
-		// % by n
-		//lookup corresponding ips]
-	}
+	Ip string
+	Port string
+}
 
-	func Get_IP(ips []string)(string, error){
-		//
+func hash(ip string, port string, n int, ips *[]string)(string,error){
+
+	//parse string by .
+	num_list := strings.FieldsFunc(ip, func(r rune) bool {
+		if r == '.' {
+			return true
+		}
+		return false
+	})
+	total_num int := strconv.Atoi(port)
+	//convert string to ints, add ints
+	for _,num := range ips {
+		new_num,err := strconv.Atoi(num)
+		if err != nil{
+			return nil, err
+		}
+		total_num += new_num
 	}
+	// % by n
+	index := total_num % n
+	//lookup corresponding ip
+	server_ip := ips[index]
+
+	return server_ip, err
+}
+
+func (ih *Iphash) Get_IP(ips *[]string)(string, error){
+	ip_lst := *ips
+	if ih.Index >= len(ip_lst) {
+		ih.Index = 0
+	}
+	ip,err := hash(ih.IP, len(ip_list), *ip_lst)
+
+	return ip, err
 }
