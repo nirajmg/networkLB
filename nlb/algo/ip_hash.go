1892f0d984e49f3fdcea4523428667cae9b79417
hlhanhl <huilin.han@colorado.edu>
2022-11-12 13:11:39 -0700

    pulling changes

---
+++ nlb/algo/ip_hash.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	//"fmt"
+	"strings"
+	"container/list"
+)
+
+type Ip_Hash struct{
+	func hash(ip string, n int)(string,error){
+		//parse string by .
+		//convert string to ints 
+		//add ints
+		// % by n
+		//lookup corresponding ips]
+	}
+
+	func Get_IP(ips []string)(string, error){
+		//
+	}
+}
